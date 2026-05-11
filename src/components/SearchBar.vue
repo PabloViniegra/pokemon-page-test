50a29ae259e5d4806627e4c7cc5d931927347215
PabloViniegra <pablovpmadrid@gmail.com>
2026-05-11 12:29:22 +0200

    add clear search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -23,5 +23,15 @@ const model = defineModel<string>({ default: '' })
         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
       />
     </svg>
+    <button
+      v-if="model"
+      type="button"
+      class="absolute right-4 top-1/2 -translate-y-1/2 w-6 h-6 flex items-center justify-center text-gray-400 hover:text-gray-600 transition-colors"
+      @click="model = ''"
+    >
+      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
   </div>
 </template>
